be3c9d9b325d95f75e3895fad5d2a70118a50751
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-06-30 17:17:03 +0700

    - add event to send message on pressing 'Enter'

--- socket-chat-client/scripts/app.js
+++ socket-chat-client/scripts/app.js
@@ -2,7 +2,14 @@ const sendButton = document.querySelector("#btnSendMsg");
 const msgArea = document.querySelector("#msgArea");
 const msgToSend = document.querySelector("#msgToSend");
 
-sendButton.addEventListener("click", () => {
+function sendMessage() {
   msgArea.value += msgToSend.value + String.fromCharCode(13, 10);
   msgToSend.value = "";
+}
+
+sendButton.addEventListener("click", sendMessage);
+msgToSend.addEventListener("keypress", event => {
+  if (event.code === "Enter") {
+    sendMessage();
+  }
 });
