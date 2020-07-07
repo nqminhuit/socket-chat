b731f13eb2774dfd54a54fa4e81203b5763e3642
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-07-07 09:30:12 +0700

    - add custom element for login form

---
+++ socket-chat-client/elements/login-form.js
@@ -0,0 +1,69 @@
+window.customElements.define(
+  "login-form",
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      const divServerAddressInput = document.createElement("div");
+      divServerAddressInput.classList.add("col-8");
+      divServerAddressInput.appendChild(
+        this.createInputText("text", "Server Ip Address", "serverAddress")
+      );
+
+      const divPortInput = document.createElement("div");
+      divPortInput.classList.add("col-4");
+      divPortInput.appendChild(this.createInputText("text", "Server Port", "serverPort"));
+
+      const divServerInfo = document.createElement("div");
+      divServerInfo.classList.add("form-row");
+      divServerInfo.appendChild(divServerAddressInput);
+      divServerInfo.appendChild(divPortInput);
+
+      const btnLogin = document.createElement("button");
+      btnLogin.classList.add("btn");
+      btnLogin.classList.add("btn-primary");
+      btnLogin.setAttribute("id", "btnLogin");
+      btnLogin.innerHTML = "Login";
+      btnLogin.addEventListener("click", function () {
+        const loginForm = document.querySelector("login-form");
+        loginForm.remove();
+        const chatbox = document.querySelector("#chatbox");
+        chatbox.style.display = "block";
+      });
+
+      const divLoginInput = document.createElement("div");
+      divLoginInput.classList.add("login-form");
+      divLoginInput.appendChild(this.createInputText("input", "Username"));
+      divLoginInput.appendChild(this.createInputText("password", "Password"));
+      divLoginInput.appendChild(document.createElement("hr"));
+      divLoginInput.appendChild(divServerInfo);
+      divLoginInput.appendChild(btnLogin);
+
+      const divLoginFormContainer = document.createElement("div");
+      divLoginFormContainer.classList.add("container");
+      divLoginFormContainer.classList.add("bordered");
+
+      const style = document.createElement("style");
+      style.textContent =
+        "@import url('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css');";
+      style.textContent += "@import url('./style.css')";
+
+      divLoginFormContainer.appendChild(divLoginInput);
+      const shadow = this.attachShadow({ mode: "open" });
+      shadow.appendChild(divLoginFormContainer);
+      shadow.appendChild(style);
+    }
+
+    createInputText(type, placeholder, id) {
+      const txtInput = document.createElement("input");
+      txtInput.classList.add("form-control");
+      txtInput.classList.add("margin-bot-10px");
+      txtInput.setAttribute("type", type);
+      txtInput.setAttribute("placeholder", placeholder);
+      if (id) {
+        txtInput.setAttribute("id", id);
+      }
+      return txtInput;
+    }
+  }
+);
--- socket-chat-client/scripts/login.js
+++
@@ -1,8 +0,0 @@
-const btnLogin = document.querySelector("#btnLogin");
-
-btnLogin.addEventListener("click", function () {
-  const loginForm = document.querySelector("#loginForm");
-  loginForm.remove();
-  const chatbox = document.querySelector("#chatbox");
-  chatbox.style.display = "block";
-});
