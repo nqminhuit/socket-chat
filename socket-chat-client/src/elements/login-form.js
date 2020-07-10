80f56c5fa96b083eea27c6b2373921dbaa76d442
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-07-10 10:55:06 +0700

    - using webpack to bundler all javascript code

---
+++ socket-chat-client/src/elements/login-form.js
@@ -0,0 +1,68 @@
+import { ChatBox } from "./chat-box.js";
+import { WebSocketHandler } from "../handlers/websocket-handler.js";
+
+export class LoginForm extends HTMLElement {
+  constructor() {
+    super();
+
+    const divServerAddressInput = document.createElement("div");
+    divServerAddressInput.classList.add("col-8");
+    divServerAddressInput.appendChild(
+      this.createInputText("text", "Server Ip Address", "serverAddress")
+    );
+
+    const divPortInput = document.createElement("div");
+    divPortInput.classList.add("col-4");
+    divPortInput.appendChild(this.createInputText("text", "Server Port", "serverPort"));
+
+    const divServerInfo = document.createElement("div");
+    divServerInfo.classList.add("form-row");
+    divServerInfo.appendChild(divServerAddressInput);
+    divServerInfo.appendChild(divPortInput);
+
+    const btnLogin = document.createElement("button");
+    btnLogin.classList.add("btn");
+    btnLogin.classList.add("btn-primary");
+    btnLogin.setAttribute("id", "btnLogin");
+    btnLogin.innerHTML = "Login";
+    btnLogin.addEventListener("click", () => {
+      this.parentElement.appendChild(new ChatBox(usernameInput.value, new WebSocketHandler()));
+      this.remove();
+    });
+
+    const divLoginInput = document.createElement("div");
+    divLoginInput.classList.add("login-form");
+    const usernameInput = this.createInputText("input", "Username");
+    divLoginInput.appendChild(usernameInput);
+    divLoginInput.appendChild(this.createInputText("password", "Password"));
+    divLoginInput.appendChild(document.createElement("hr"));
+    divLoginInput.appendChild(divServerInfo);
+    divLoginInput.appendChild(btnLogin);
+
+    const divLoginFormContainer = document.createElement("div");
+    divLoginFormContainer.classList.add("container");
+    divLoginFormContainer.classList.add("bordered");
+
+    const style = document.createElement("style");
+    style.textContent = "@import url('./styles/login-form.css')";
+
+    divLoginFormContainer.appendChild(divLoginInput);
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(divLoginFormContainer);
+    shadow.appendChild(style);
+  }
+
+  createInputText(type, placeholder, id) {
+    const txtInput = document.createElement("input");
+    txtInput.classList.add("form-control");
+    txtInput.classList.add("margin-bot-10px");
+    txtInput.setAttribute("type", type);
+    txtInput.setAttribute("placeholder", placeholder);
+    if (id) {
+      txtInput.setAttribute("id", id);
+    }
+    return txtInput;
+  }
+}
+
+window.customElements.define("login-form", LoginForm);
