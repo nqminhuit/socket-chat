1bdc1132fa7bf8a716da10f6a4c1071407d2475d
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-07-13 11:53:15 +0700

    - send message on clicking "Send" button

--- socket-chat-client/src/app/jsx/chat-box.jsx
+++ socket-chat-client/src/app/jsx/chat-box.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Input } from "./input.jsx";
 import "../../styles/chat-box.css";
 import { WebSocketHandler } from "../handlers/websocket-handler.js";
+import { Input } from "./input.jsx";
 
 export class ChatBox extends React.Component {
   constructor() {
@@ -9,6 +9,7 @@ export class ChatBox extends React.Component {
     this.state = {
       message: "",
     };
+    this.sendMessage = this.sendMessage.bind(this);
     this.handleInputMessage = this.handleInputMessage.bind(this);
     this.handleOnKeyPressInput = this.handleOnKeyPressInput.bind(this);
   }
@@ -25,18 +26,18 @@ export class ChatBox extends React.Component {
     this.webSocket = null;
   }
 
-  sendMessage(msg) {
-    const procMsg = msg.trim();
+  sendMessage() {
+    const procMsg = this.state.message.trim();
     if (procMsg.length === 0) {
       return;
     }
     this.webSocket.sendMessage(`${this.props.username}: ${procMsg}`);
+    window.document.querySelector("#msgToSend").value = "";
   }
 
   handleOnKeyPressInput(key) {
     if (key === "Enter") {
-      this.sendMessage(this.state.message);
-      window.document.querySelector("#msgToSend").value = "";
+      this.sendMessage();
     }
   }
 
@@ -58,7 +59,11 @@ export class ChatBox extends React.Component {
             onKeyPressInput={this.handleOnKeyPressInput}
           />
           <div className="input-group-append">
-            <button id="btnSendMsg" className="btn btn-outline-secondary">
+            <button
+              id="btnSendMsg"
+              className="btn btn-outline-secondary"
+              onClick={this.sendMessage}
+            >
               Send
             </button>
           </div>
