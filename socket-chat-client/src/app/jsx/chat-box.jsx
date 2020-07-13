9183260dd6dbcb5c14cb55eadedff6c7e9a95719
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-07-13 09:58:49 +0700

    - add event handler for keypress in Input - apply onKeyPress to Chatbot's Input

--- socket-chat-client/src/app/jsx/chat-box.jsx
+++ socket-chat-client/src/app/jsx/chat-box.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Input } from "./input.jsx";
 import "../../styles/chat-box.css";
 
 export class ChatBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "",
+    };
+    this.handleInputMessage = this.handleInputMessage.bind(this);
+    this.handleOnKeyPressInput = this.handleOnKeyPressInput.bind(this);
+  }
+
+  sendMessage(msg) {
+    console.log(msg);
+  }
+
+  handleOnKeyPressInput(key) {
+    if (key === "Enter") {
+      this.sendMessage(this.state.message);
+      window.document.querySelector("#msgToSend").value = "";
+    }
+  }
+
+  handleInputMessage(msg) {
+    this.setState({ message: msg });
+  }
+
   render() {
     return (
       <div id="chatbox" className="container">
@@ -9,7 +34,12 @@ export class ChatBox extends React.Component {
           <textarea id="msgArea" disabled className="form-control no-resize"></textarea>
         </div>
         <div className="input-group mb-3">
-          <input id="msgToSend" type="text" placeholder="message" className="form-control" />
+          <Input
+            id="msgToSend"
+            placeholder="Message"
+            onInputChange={this.handleInputMessage}
+            onKeyPressInput={this.handleOnKeyPressInput}
+          />
           <div className="input-group-append">
             <button id="btnSendMsg" className="btn btn-outline-secondary">
               Send
--- socket-chat-client/src/app/jsx/input.jsx
+++ socket-chat-client/src/app/jsx/input.jsx
@@ -4,22 +4,34 @@ export class Input extends React.Component {
   constructor(props) {
     super(props);
     this.handleValueChange = this.handleValueChange.bind(this);
+    this.handleOnKeyPress = this.handleOnKeyPress.bind(this);
   }
 
   handleValueChange(event) {
     this.props.onInputChange(event.target.value);
   }
 
+  handleOnKeyPress(event) {
+    this.props.onKeyPressInput(event.key);
+  }
+
   render() {
-    const inputValue = this.props.inputValue;
-    return (
+    return !this.props.onKeyPressInput ? (
       <input
         id={this.props.id}
         className="form-control margin-bot-10px"
         type={this.props.type ? this.props.type : "text"}
         placeholder={this.props.placeholder}
-        value={inputValue}
         onChange={this.handleValueChange}
+      ></input>
+    ) : (
+      <input
+        id={this.props.id}
+        className="form-control margin-bot-10px"
+        type={this.props.type ? this.props.type : "text"}
+        placeholder={this.props.placeholder}
+        onChange={this.handleValueChange}
+        onKeyPress={this.handleOnKeyPress}
       ></input>
     );
   }
